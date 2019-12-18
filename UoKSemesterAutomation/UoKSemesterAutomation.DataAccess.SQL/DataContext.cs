81a51dadd5452edcf04bb693d6da7567397a1cb1
Rimsha <Rimsha@RIMSHA-PC>
2019-12-18 21:26:22 +0500

    Connect Database

---
+++ UoKSemesterAutomation/UoKSemesterAutomation.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UoKSemesterAutomation.Core.Models;
+
+namespace UoKSemesterAutomation.DataAccess.SQL
+{
+    class DataContext : DbContext
+    {
+        public DataContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Student> students { get; set; }
+        public DbSet<Department> departments { get; set; }
+    
+    }
+}
